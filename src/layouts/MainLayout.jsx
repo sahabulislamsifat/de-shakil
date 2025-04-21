6c1350f271b53c777aa3e29f7280d5f52a6904d2
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-04-22 02:17:54 +0600

    project setup

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/header/Header";
+import Navbar from "../components/navbar/Navbar";
+
+const MainLayout = () => {
+  return (
+    <>
+      {/* Navbar  */}
+      <Navbar></Navbar>
+      {/* Header */}
+      <Header></Header>
+    </>
+  );
+};
+
+export default MainLayout;
